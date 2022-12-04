bdd6d6ad25dd59aad7e34797fc96d75fd8a61de6
ayuubb <ayubbkj2@gmail.com>
2022-12-04 16:50:51 +0700

    update

--- config/database.js
+++ config/database.js
@@ -4,11 +4,18 @@ const nameDatabase = process.env.MYSQLDATABASE || 'db';
 const user = process.env.MYSQLUSER || 'root';
 const host = process.env.MYSQLHOST || 'localhost';
 const password = process.env.MYSQLPASSWORD || '';
+const port = process.env.MYSQLPORT || 3306;
+// const nameDatabase = process.env.MYSQLDATABASE;
+// const user = process.env.MYSQLUSER;
+// const host = process.env.MYSQLHOST;
+// const password = process.env.MYSQLPASSWORD;
+
 
 
 const db = new Sequelize(nameDatabase,user,password,{
     host: host,
-    dialect: "mysql"
+    dialect: "mysql",
+    port : port
 });
 
 export default db;
